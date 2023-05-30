23e89af6015204a4ca69e5ab6b2900c09784ba88
Александр <perchatki79@gmail.com>
2023-05-30 20:59:20 +0300

    Initial commit

---
+++ src/test/java/pro/sky/algoritm2/Algoritm2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.algoritm2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Algoritm2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
